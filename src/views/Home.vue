957e8e5e2ba29fea9c372e0e6addc1d95c895c81
Jasen Peterson <jasen@ellcreative.com>
2018-08-27 15:47:10 -0500

    Implemented progress bar and "hi" in/out transitions.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="flex min-h-screen page page__home">
-    <!-- <div v-for="(pageContent, i) in content" :key="i">
-      <h1 class="sm:text-lg md:text-4xl lg:text-5xl">{{ pageContent.data.home_content[i].text }}</h1>
-    </div> -->
     <div class="container container__home flex-1 self-center text-center">
-        <h1 class="sm:text-lg md:text-4xl lg:text-5xl">Hi.</h1>
+        <h1 class="sm:text-lg md:text-4xl lg:text-5xl" v-bind:class="{ remove: dataHasLoaded }">Hi.</h1>
     </div>
   </div>
 </template>
@@ -14,13 +11,39 @@ import { mapGetters } from "vuex";
 
 export default {
   name: "home",
+  data() {
+    return {
+      dataHasLoaded: false
+    };
+  },
   mounted: function() {
     this.$store.dispatch("getPage", "home");
   },
   computed: {
     ...mapGetters({
-      content: "pageContent"
+      content: "pageContent",
+      isLoading: "isLoading"
     })
+  },
+  watch: {
+    isLoading(val) {
+      if (val == false) {
+        let self = this;
+        setTimeout(function() {
+          self.dataHasLoaded = true;
+        }, 1000);
+      }
+    }
   }
 };
 </script>
+<style scoped>
+h1 {
+  animation: slide-in-bottom 0.8s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
+}
+
+h1.remove {
+  animation: slide-out-bottom 0.8s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
+  animation-delay: 0.5s;
+}
+</style>
